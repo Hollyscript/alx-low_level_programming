47827356a73a34ac5a523da44dd358a1739e570a
odunola <odunoladimeji972@gmail.com>
2023-04-20 18:36:39 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,6 +4,6 @@
  * Return: 0 (code is working)
  */
 int main(void)
-{printf("with proper grammar, but the outcome is a piece of art, \n");
+{printf("with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
