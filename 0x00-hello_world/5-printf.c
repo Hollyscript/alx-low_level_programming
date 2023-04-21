4fb55fd41dfc46478dfdece0b6b112835d3bbd6b
odunola <odunoladimeji972@gmail.com>
2023-04-20 18:33:09 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with printf function
+ * Return: 0 (code is working)
+ */
+int main(void)
+{printf("with proper grammar, but the outcome is a piece of art, \n");
+return (0);
+}
