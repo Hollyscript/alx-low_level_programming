5d8ced9db18cc2a30efe6e471ebefd799de4b6cf
odunola <odunoladimeji972@gmail.com>
2023-04-24 04:59:26 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints numbers fro 0 to 9
+ *
+ * Return: Always 0 (code is working)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
