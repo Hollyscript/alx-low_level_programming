f743016c05e4710add3ce56ddae294fc97affb2f
odunola <odunoladimeji972@gmail.com>
2023-04-25 15:11:16 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet in lowercase ten times
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+	int a;
+	int b;
+
+	for (a = 1; a <= 10; a++)
+	{
+		for (b = 'a'; b <= 'z'; b++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n');
+	}
+}
+
